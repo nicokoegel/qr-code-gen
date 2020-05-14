cd93afeb98905585496cfa3c8bbefc303800c0fb
nicokoegel <koegel99@gmail.com>
2020-05-14 11:24:30 +0200

    Updates

---
+++ precache-manifest.723076f3fe1a4521fbaf0a185295a1ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de1978d71722c27bc9d8d85dd20cfff3",
+    "url": "/qr-code-gen/index.html"
+  },
+  {
+    "revision": "4645377946d99a86fb1f",
+    "url": "/qr-code-gen/static/css/main.d3c3a7eb.chunk.css"
+  },
+  {
+    "revision": "41e913f8daad98fb8430",
+    "url": "/qr-code-gen/static/js/2.789e08b9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/qr-code-gen/static/js/2.789e08b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4645377946d99a86fb1f",
+    "url": "/qr-code-gen/static/js/main.d70a4de8.chunk.js"
+  },
+  {
+    "revision": "8eedf4e3f4237110769f",
+    "url": "/qr-code-gen/static/js/runtime-main.debc4dcc.js"
+  }
+]);
