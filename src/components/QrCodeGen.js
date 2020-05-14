278e71d3b19ce22e986bbac4e3fac756b826d85e
nicokoegel <koegel99@gmail.com>
2020-05-14 11:09:23 +0200

    fixed TexTfield and added download icon

--- src/components/QrCodeGen.js
+++ src/components/QrCodeGen.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
-import TextField from '';
+import { Box, TextField } from '@material-ui/core';
 import QRCode from 'qrcode.react';
 import './QrCodeGen.css'
+import downloadIcon from '../assets/noun_Download_2176275.png'
 
 export const QrCodeGen = () => {
     const [qrCodeValue, setQrValue] = useState('nicokoegel.github.io/qr-code-gen');
@@ -20,21 +21,32 @@ export const QrCodeGen = () => {
 
     return (
         <div>
-            <input type="text" onChange={event => setQrValue(event.target.value)} placeholder={qrCodeValue}></input>
+            <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                className="box"
+            >
+                <TextField
+                    id="inputfield"
+                    label="URL-Adresse"
+                    placeholder={qrCodeValue}
+                    variant="outlined"
+                    onChange={event => setQrValue(event.target.value)}
+                    style={{ width: 400 }}
+                />
+            </Box>
             <QRCode
                 id="QrCode"
                 value={qrCodeValue}
                 size={290}
                 level={"L"}
-                includeMargin={true}
+                includeMargin={false}
             />
-            <a onClick={downloadQR} className="link"> Download QR </a>
-            <TextField
-                id="outlined-required"
-                label="URL-Adresse"
-                defaultValue="Hello World"
-                variant="outlined"
-            />
+            <a onClick={downloadQR} href=" " className="link">
+                <img src={downloadIcon} alt="download icon" width="20px" height="20px" />
+                &nbsp;Download QR-Code
+            </a>
         </div>
     )
 }
